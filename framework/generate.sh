6b0e2a6dccd2bf3275daa172c1103ffa99036592
Charlie Mordant <cmordant1@gmail.com>
2025-03-09 21:23:00 +0100

    more characters due to markdown conversion

--- framework/generate.sh
+++ framework/generate.sh
@@ -4,10 +4,11 @@ get_prompt() {
   echo "Getting the prompt to send from asciidoctor story"
   local src="$1"
   local dst="$2"
-  asciidoctor -a allow-uri-read -b html5 -d inline -q -s "$src/**/backlog/*.adoc" -o -
+  asciidoctor -a allow-uri-read -b html5 -d inline -q -s "$src/**/backlogs/*.adoc" -o -
 }
-# Set source and destination directories
-query_llm() {
+
+# Returns the prompt
+query_gemini_llm() {
   local bruno_path="./engine/bruno/prompt-executor"
   local prompt="$1"
   local key="$2"
@@ -15,27 +16,26 @@ query_llm() {
   mkdir "$cwd" || true
   local tmpfile="$cwd"/tmp.json
   rm "$tmpfile" || true
-  (cd $bruno_path && bru run --output "$tmpfile" --env prod --env-var promptText="$prompt" --env-var apiKey="$key")
+  (cd $bruno_path && bru run getGeminiLLMResponse.bru --output "$tmpfile" --env prod --env-var promptText="$prompt" --env-var apiKey="$key")
   # shellcheck disable=SC2002
   ret_val=$(cat "$tmpfile" | jq '.[0].results.[0].response.data.candidates.[0].content.parts.[0].text')
 }
 
 get_inline_prompt() {
-  echo "Getting the inline prompt to send from asciidoctor story"
   local file="$1"
-  ret_val=$(asciidoctor -a allow-uri-read -b html5 -d inline -q -s "$file" -o - | tr -s '\n' ' ')
+  ret_val=$(asciidoctor -a allow-uri-read -b html5 -q -e "$file" -o - | tr -s '"' "'" | pandoc --from html --to markdown_strict -o - | tr -s '\n' ' ')
 }
 
 iterate_over_backlog() {
   echo "Iterate over backlog file"
   local src="$1"
   local dst="$2"
-  for file in "$src"/contributor/backlog/*; do
+  for file in "$src"/prompts/contributors/backlogs/*; do
     if [[ -f "$file" ]]; then
       get_inline_prompt "$file"
       apiKey="$GEMINI_KEY"
       prompt=$ret_val
-      query_llm "$prompt" "$apiKey"
+      query_gemini_llm "$prompt" "$apiKey"
       rm "$dst/$file" || true
       mkdir -p $(dirname "$dst/$file")
       echo "$ret_val" > "$dst/$file"
--- framework/setup.sh
+++ framework/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-brew install asciidoctor
+brew install asciidoctor pandoc
 npm install -g @usebruno/cli
